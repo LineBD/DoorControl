91bfb80d44cf94a6e686e66c4d85ad85d61a95df
kristinlieaas <kristinlieaas@gmail.com>
2018-02-22 11:50:25 +0100

    metode

--- DoorControl/UserValidation.cs
+++ DoorControl/UserValidation.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace DoorControl
 {
-    class UserValidation
+    public class UserValidation
     {
+        public void ValidateEntryRequest(int id)
+        {
+
+        }
     }
 }
